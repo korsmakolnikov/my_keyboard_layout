e5a50313b18f437501696119e482940ac8233be7
korsmakolnikov@gmail.com <korsmakolnikov@gmail.com>
2021-02-18 20:23:30 +0100

    Permanent ctrl caps lock swap

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 sudo cp /usr/share/X11/xkb/symbols/us /usr/share/X11/xkb/symbols/us.bak
 sudo cp us /usr/share/X11/xkb/symbols/
 setxkbmap -layout us
+
+sudo cp ./etc.default.keyboard /etc/default/keyboard
