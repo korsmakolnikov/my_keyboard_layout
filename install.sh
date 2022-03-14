c192e9e333965a3a3584a69af616767666e99712
korsmakolnikov <korsmakolnikov@gmail.com>
2022-03-14 10:39:01 +0100

    Fix Arch

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo cp us /usr/share/X11/xkb/symbols/
 setxkbmap -layout us
 
 # Set fn keys to normal behavior
-echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
+# echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
 
 sudo cp ./etc.default.keyboard /etc/default/keyboard
