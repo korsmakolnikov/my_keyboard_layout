12e18781d84d0cc29e970808ea028b31226ad077
korsmakolnikov <korsmakolnikov@gmail.com>
2022-03-30 13:03:19 +0200

    Fix script

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 sudo cp /usr/share/X11/xkb/symbols/us /usr/share/X11/xkb/symbols/us.bak
-sudo cp us /usr/share/X11/xkb/symbols/
+sudo cp ./etc.share.X11.symbols.us /usr/share/X11/xkb/symbols/us
 setxkbmap -layout us
 
-# Set fn keys to normal behavior
-# echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
-
-sudo cp ./etc.default.keyboard /etc/default/keyboard
